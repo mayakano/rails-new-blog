38c33d812fcf31344d305981c39735c8941cb0d8
Maya Kano <mayakanoweb@gmail.com>
2018-12-26 03:11:18 -0500

    adding authentication

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
+    http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+   
     def index
-        @articles = Article.all
+      @articles = Article.all
     end
     
 
